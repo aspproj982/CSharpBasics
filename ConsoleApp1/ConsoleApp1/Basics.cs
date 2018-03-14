c21926e4112d8acd54c5fa87b03b07c2ceab86ff
asp6f8 <apsynthes@gmail.com>
2018-03-13 20:40:09 -0500

    Added files for Basics assignment
    These are all the files created for the C# Basics assignment for CS4330

---
+++ ConsoleApp1/ConsoleApp1/Basics.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const byte sample1 = 0x3A;
+            byte sample2 = 58;
+            int heartRate = 85;
+            var deposits = 135002796.0;
+            const float acceleration = 9.800F;
+            var mass = 14.6F;
+            var distance = 129.763001;
+            var lost = true;
+            var expensive = true;
+            var choice = 2;
+            const char integral = '\u222B';
+            const String greeting = "Hello";
+            var name = "Karen";
+
+            if (sample1 == sample2) Console.WriteLine("The samples are equal.");
+            else Console.WriteLine("The samples are not equal.");
+
+            if (heartRate >= 40 && heartRate <= 80) Console.WriteLine("Heart rate is normal.");
+            else Console.WriteLine("Heart rate is not normal.");
+
+            if (deposits >= 100000000) Console.WriteLine("You are exceedingly wealthy.");
+            else Console.WriteLine("Sorry you are so poor.");
+
+            var force = mass * acceleration;
+            Console.WriteLine($"force = {force}");
+
+            Console.WriteLine($"{distance} is the distance.");
+
+            if ((lost) && (expensive))
+                Console.WriteLine("I am really sorry! I will get the manager.");
+            else if ((lost) && (!expensive))
+                Console.WriteLine("Here is a coupon for 10% off.");
+
+            switch(choice){
+                case 1:
+                    Console.WriteLine("You chose 1.");
+                    break;
+                case 2:
+                    Console.WriteLine("You chose 2.");
+                    break;
+                case 3:
+                    Console.WriteLine("You chose 3.");
+                    break;
+                default:
+                    Console.WriteLine("You made an unknown choice.");
+                    break;
+            }
+
+            Console.WriteLine($"{integral} is an integral.");
+
+            for (int i = 5; i <= 10; i++)
+            {
+                Console.WriteLine($"i={i}");
+            }
+
+            var age = 0;
+            while (age < 6)
+            {
+                Console.WriteLine($"age = {age}");
+                age++;
+            }
+
+            Console.WriteLine($"{greeting} {name}");
+            Console.ReadKey();
+        }
+    }
+}
